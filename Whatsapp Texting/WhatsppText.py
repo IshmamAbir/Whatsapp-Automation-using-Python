c5b0cf74b0c8865758b51473be8e53f9448cc4db
Ishmam Abir <ishmam130@gmail.com>
2021-06-14 02:10:40 +0600

    class name changed in updated version, fixing search issue

--- Whatsapp Texting/WhatsppText.py
+++ Whatsapp Texting/WhatsppText.py
@@ -6,42 +6,41 @@ from selenium.webdriver.common.keys import Keys
 import time
 from time import sleep
 
-browser= webdriver.Chrome("D:\PRACTISE CODE\Python Practise\Whatspp Access Texter\chromedriver.exe")
+browser = webdriver.Chrome("D:\PRACTISE CODE\Python Practise\Whatspp Access Texter\chromedriver.exe") #update driver according to your chrome version, or else it will not run
 browser.get('http://web.whatsapp.com')
-wait= WebDriverWait(browser,60)
+wait = WebDriverWait(browser, 60)
 
-target= '"Tahsin"' #senders name // for user input , input("Type the target name")
-string = " hello️" #message that will send // for user input of text message , input("Type the message you want to send")
-x_arg = '//span[contains(@title,'+target+')]'
+target = '"safin"'  # senders name // for user input , input("Type the target name")
+string = " egulai automation B) "  # message that will send // for user input of text message , input("Type the message you want to send")
+x_arg = '//span[contains(@title,' + target + ')]'
 target = wait.until(ec.presence_of_element_located((By.XPATH, x_arg)))
 target.click()
 
-input_box= browser.find_element_by_class_name('_3uMse')
-for i in range(52):
-    input_box.send_keys(str(i)+string + Keys.ENTER ) #str(i)+
+input_box = browser.find_elements_by_class_name('_2_1wd')[1] #for multiple class name sharing the same name, [1] is the index number,for single class name, find_Element_By_Class_Name
+for i in range(173):
+    input_box.send_keys(str(i) + string + Keys.ENTER)
 
-#Audio file send
+# Audio file send
 
-attachment_section = browser.find_element_by_xpath('//div[@title= "Attach"]') #searching attachment section in chat
-attachment_section.click()
+# attachment_section = browser.find_element_by_xpath('//div[@title= "Attach"]') #searching attachment section in chat
+# attachment_section.click()
+#
+# audio_file= browser.find_element_by_xpath('//input[@accept="image/*,video/mp4,video/3gpp,video/quicktime"]')  # search image add button
+# audio_file.send_keys('C:/Users/ASUS-PC/Downloads/Music/Imagine Dragons - Bad Liar.mp3') #adding image path // for user input path, input('Please enter the file path')
+#
+# sleep(3)
+# send_button  = browser.find_element_by_xpath('//span[@data-icon="send"]')
+# send_button.click()
 
-audio_file= browser.find_element_by_xpath('//input[@accept="image/*,video/mp4,video/3gpp,video/quicktime"]')  # search image add button
-audio_file.send_keys('C:/Users/ASUS-PC/Downloads/Music/Imagine Dragons - Bad Liar.mp3') #adding image path // for user input path, input('Please enter the file path')
 
-sleep(3)
-send_button  = browser.find_element_by_xpath('//span[@data-icon="send"]')
-send_button.click()
+# Image send
 
-
-#Image send
-
-attachment_section = browser.find_element_by_xpath('//div[@title= "Attach"]') #searching attachment section in chat
-attachment_section.click()
-
-image_file= browser.find_element_by_xpath('//input[@accept="image/*,video/mp4,video/3gpp,video/quicktime"]')  # search image add button
-image_file.send_keys('C:/Users/ASUS-PC/Downloads/images.jpg') #adding image path // for user input path, input('Please enter the file path')
-
-sleep(3)
-send_button  = browser.find_element_by_xpath('//span[@data-icon="send"]')
-send_button.click()
-
+# attachment_section = browser.find_element_by_xpath('//div[@title= "Attach"]') #searching attachment section in chat
+# attachment_section.click()
+#
+# image_file= browser.find_element_by_xpath('//input[@accept="image/*,video/mp4,video/3gpp,video/quicktime"]')  # search image add button
+# image_file.send_keys('C:/Users/ASUS-PC/Downloads/images.jpg') #adding image path // for user input path, input('Please enter the file path')
+#
+# sleep(3)
+# send_button  = browser.find_element_by_xpath('//span[@data-icon="send"]')
+# send_button.click()
